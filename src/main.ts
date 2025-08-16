d7555fcbe34feee53342fdcb48f70b4458c1e8da
Juan Aguirre <j1927aguirre@gmail.com>
2025-08-16 16:02:42 -0500

    feat: initialize NestJS application with basic structure
    - Created app.module.ts to define the main application module.
    - Implemented main.ts to bootstrap the application with global settings.
    - Added e2e tests in app.e2e-spec.ts to verify the root endpoint.
    - Configured Jest for e2e testing with jest-e2e.json.
    - Set up TypeScript configuration for building the project in tsconfig.build.json.
    - Defined main TypeScript configuration in tsconfig.json with strict settings.

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+  logger.log('Starting application...');
+  const app = await NestFactory.create(AppModule);
+
+  // Global prefix for routes
+  app.setGlobalPrefix('api');
+  // Setting Global Validation Pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        exposeUnsetFields: false,
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 3000);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
