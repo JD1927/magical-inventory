1752260c354cc0dd3f91bc07ccb5f307866bb9e4
Juan Aguirre <j1927aguirre@gmail.com>
2025-08-24 21:44:53 -0500

    feat: add InventoryMovement and Inventory entities for inventory management

---
+++ src/inventory/entities/inventory-movements.entity.ts
@@ -0,0 +1,61 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { DecimalTransformer } from '../../common/transformers/numeric.transformer';
+import { Product } from '../../products/entities/product.entity';
+import { Supplier } from '../../suppliers/entities/supplier.entity';
+
+export enum EMovementType {
+  IN = 'IN',
+  OUT = 'OUT',
+}
+
+@Entity('inventory_movements')
+export class InventoryMovement {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Product, { eager: true })
+  @JoinColumn({ name: 'product_id' })
+  product: Product;
+
+  @Column({ type: 'enum', enum: EMovementType })
+  type: EMovementType;
+
+  @Column({ type: 'int' })
+  quantity: number;
+
+  @Column({
+    type: 'numeric',
+    precision: 10,
+    scale: 2,
+    nullable: true,
+    transformer: new DecimalTransformer(),
+  })
+  purchasePrice?: number;
+
+  @Column({
+    type: 'numeric',
+    precision: 10,
+    scale: 2,
+    nullable: true,
+    transformer: new DecimalTransformer(),
+  })
+  salePrice?: number;
+
+  @ManyToOne(() => Supplier, {
+    nullable: true,
+    eager: true,
+    onDelete: 'SET NULL',
+  })
+  @JoinColumn({ name: 'supplier_id' })
+  supplier: Supplier | null;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+}
---
+++ src/inventory/entities/inventory.entity.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Product } from '../../products/entities/product.entity';
+import { DecimalTransformer } from '../../common/transformers/numeric.transformer';
+
+@Entity({ name: 'inventory' })
+export class Inventory {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Product, { eager: true })
+  @JoinColumn({ name: 'product_id' })
+  product: Product;
+
+  @Column({ type: 'int', default: 0 })
+  stock: number;
+
+  @Column({
+    name: 'average_cost',
+    type: 'numeric',
+    precision: 10,
+    scale: 2,
+    default: 0,
+    transformer: new DecimalTransformer(),
+  })
+  averageCost: number; // COGS - Cost Of Goods Sold
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
